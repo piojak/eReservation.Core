afb7bafb280866789f1b36dc54eb48cf19536006
CUK\lhryciuk <l.hryciuk@cuk.pl>
2017-12-17 22:30:21 +0100

    Bootstrap 4 + popper + SASS

---
+++ eReservation/Gruntfile.js
@@ -0,0 +1,38 @@
+﻿/// <binding AfterBuild='sasscomp' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Sass
+        sass: {
+            options: {
+                sourceMap: true, // Create source map
+                outputStyle: 'compressed' // Minify output
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true, // Recursive
+                        cwd: "Styles", // The startup directory
+                        src: ["**/*.scss"], // Source files
+                        dest: "wwwroot/css", // Destination
+                        ext: ".css" // File extension
+                    }
+                ]
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-sass');
+
+    // Default task(s).
+    grunt.registerTask('sasscomp', ['sass']);
+};
